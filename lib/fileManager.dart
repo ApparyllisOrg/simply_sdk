15c524514f3d1dfea0e84dbf7db185bcad6b7874
SaltyPandaStudios <accounts@saltypandastudios.com>
2021-06-23 20:07:42 +0200

    fix filemanager

--- lib/fileManager.dart
+++ lib/fileManager.dart
@@ -1,9 +1,4 @@
-import 'package:amplify_core/amplify_core.dart';
-import 'package:amplify_storage_s3/amplify_storage_s3.dart';
-
 class FileManager {
-  final AmplifyStorageS3 storage = AmplifyStorageS3();
-
   FileManager() {}
 
   void LoadImage(String id, String uid) {}
