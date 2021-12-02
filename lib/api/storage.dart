ab0e5220fdab8b7ba27eed8879916d1f39bfff98
Amaryllis <accounts@saltypandastudios.com>
2021-12-02 18:46:24 +0100

    update storage

--- lib/api/storage.dart
+++ lib/api/storage.dart
@@ -7,8 +7,10 @@ import '../simply_sdk.dart';
 class Storage {
   Future<bool> storeAvatar(String avatarUuid, Uint8List bytes) async {
     try {
-      await delete(Uri.parse(
-          API().connection().getRequestUrl("v1/avatar/$avatarUuid", "")));
+      await post(
+          Uri.parse(
+              API().connection().getRequestUrl("v1/avatar/$avatarUuid", "")),
+          body: {"buffer": bytes});
 
       return true;
     } catch (e) {}
