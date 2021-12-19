f6cd043ab53010dea21112accb94f06586ebc3c2
Celeste <celeste@saltypandastudios.com>
2021-12-19 16:31:56 +0100

    add change listener

--- lib/modules/collection.dart
+++ lib/modules/collection.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:simply_sdk/types/document.dart';
 
 abstract class DocumentData {
@@ -8,9 +9,25 @@ abstract class DocumentData {
 abstract class Collection {
   String type = "NONE";
 
+  List<ValueChanged<Document<DocumentData>>?> boundChanges = [];
+
   Future<Document<DocumentData>> get(String id);
   Future<List<Document<DocumentData>>> getAll();
   Document<DocumentData> add(DocumentData values);
   void update(String documentId, DocumentData values);
   void delete(String documentId);
+
+  void listenForChanges(ValueChanged<Document<DocumentData>> bindFunc) {
+    boundChanges.add(bindFunc);
+  }
+
+  void cancelListenForChanges(ValueChanged<Document<DocumentData>> bindFunc) {
+    boundChanges.remove(bindFunc);
+  }
+
+  void propogateChanges(Document<DocumentData> change) {
+    boundChanges.forEach((element) {
+      if (element != null) element(change);
+    });
+  }
 }
