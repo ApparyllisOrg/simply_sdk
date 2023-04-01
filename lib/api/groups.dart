42cf2d154f3e15783eeed737aa667eed138dd62b
Amaryllis <celeste@saltypandastudios.com>
2023-04-01 17:21:50 +0200

    fix groups

--- lib/api/groups.dart
+++ lib/api/groups.dart
@@ -66,14 +66,17 @@ class Groups extends Collection<GroupData> {
 
   @override
   Future<Document<GroupData>> get(String id) async {
-    return getSimpleDocument(id, 'v1/group/${API().auth().getUid()}', type, (data) => GroupData()..constructFromJson(data.content), () => GroupData());
+    return getSimpleDocument(
+        id, 'v1/group/${API().auth().getUid()}', type, (data) => GroupData()..constructFromJson(data.content), () => GroupData());
   }
 
   @override
   Future<List<Document<GroupData>>> getAll({String? uid, int? since, bool bForceOffline = false}) async {
-    final collection = await getCollection<GroupData>('v1/groups/${uid ?? API().auth().getUid()}', '', type, since: since, bForceOffline: bForceOffline);
+    final collection =
+        await getCollection<GroupData>('v1/groups/${uid ?? API().auth().getUid()}', '', type, since: since, bForceOffline: bForceOffline);
 
-    List<Document<GroupData>> groups = collection.data.map<Document<GroupData>>((e) => Document(e['exists'], e['id'], GroupData()..constructFromJson(e['content']), type)).toList();
+    List<Document<GroupData>> groups =
+        collection.data.map<Document<GroupData>>((e) => Document(e['exists'], e['id'], GroupData()..constructFromJson(e['content']), type)).toList();
     if (!collection.useOffline) {
       if ((uid ?? API().auth().getUid()) == API().auth().getUid()) {
         API().cache().clearTypeCache(type);
@@ -95,13 +98,10 @@ class Groups extends Collection<GroupData> {
     });
   }
 
-  Future<void> setGroupsForMember(String member, List<String> groups) async
-  {
-    final data = {"member": member, "groups":  groups};
-    final Map<String, dynamic> jsonPayload = jsonEncode(data) as Map<String, dynamic>;
-    
-    API()
-      .network()
-      .request(NetworkRequest(HttpRequestMethod.Patch, 'v1/group/members', DateTime.now().millisecondsSinceEpoch, payload: jsonPayload));
+  Future<void> setGroupsForMember(String member, List<String> groups) async {
+    final data = {"member": member, "groups": groups};
+    final Map<String, dynamic> jsonPayload = data as Map<String, dynamic>;
+
+    API().network().request(NetworkRequest(HttpRequestMethod.Patch, 'v1/group/members', DateTime.now().millisecondsSinceEpoch, payload: jsonPayload));
   }
 }
